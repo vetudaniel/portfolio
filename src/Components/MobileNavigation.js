8b8b80d4e12f71504e137c731322f57b282b5fd5
daniel vetu <vetu.daniel@gmail.com>
2021-12-15 08:59:27 +0100

    fixed netlify build

--- src/Components/MobileNavigation.js
+++ src/Components/MobileNavigation.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './MobileNavigation.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faHome, faInfoCircle, faAddressBook } from '@fortawesome/free-solid-svg-icons'
 
 export function MobileNavigation() {
